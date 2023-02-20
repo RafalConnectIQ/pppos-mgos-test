c2b891949287857f5f9ef78a57b50682023ede19
Rafal <rafal.pernal@and-tech.pl>
2023-02-20 16:03:03 +0000

    11

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "mgos.h"
+#include "mgos_pppos.h"
 
 enum mgos_app_init_result mgos_app_init(void)
 {
-    mgos_gpio_set_mode(15, MGOS_GPIO_MODE_OUTPUT);
-    mgos_gpio_write(15, false);
-    mgos_msleep(4000); // sleeping for 4 seconds to turn on gsm modem
-    mgos_gpio_write(15, true);
+    // esp_log_level_set("esp-netif_lwip-ppp", ESP_LOG_VERBOSE);
+    mgos_gpio_set_mode(21, MGOS_GPIO_MODE_OUTPUT);
+    mgos_gpio_write(21, true);
+    mgos_msleep(15000); // sleeping for 4 seconds to turn on gsm modem
+    mgos_pppos_connect(0 /* if_instance */);
     return MGOS_APP_INIT_SUCCESS;
 }
